e533a4c2d4b9d901765a4192af7cafcbd0f4a8f9
Raider <gabrielnae1@gmail.com>
2023-05-22 22:10:54 +0200

    Affichage Texte

---
+++ Programme/application.ts
@@ -0,0 +1,36 @@
+import * as rs from "readline-sync";
+
+import { StationMétéo } from "./station-meteo";
+import { AfficheurTexte } from "./afficheur-texte";
+import { AfficheurGraphique } from "./afficheur-graphique";
+//Permet l'affhichage de la météo version graphique
+function main(): void {
+  const aff = new AfficheurTexte();
+  const aff2 = new AfficheurGraphique();
+  const stationMeteo = new StationMétéo(10);
+
+  console.log("Appuyez sur entrée pour le prochain affichage");
+  rs.question();
+  aff.affiche(stationMeteo.temperature, stationMeteo.humidité)
+
+  stationMeteo.humidité++;
+  for (let p = 0; p < 10; p++) {
+    console.log("Appuyez sur entrée pour le prochain affichage");
+    rs.question();
+    aff.affiche(stationMeteo.temperature, stationMeteo.humidité)
+    //affichage graphique
+    stationMeteo.humidité--;
+    stationMeteo.temperature += 2;
+  }
+
+  for (let p = 0; p < 10; p++) {
+    console.log("Appuyez sur entrée pour le prochain affichage");
+    rs.question();
+    aff.affiche(stationMeteo.temperature, stationMeteo.humidité)
+    //affichage graphique
+    stationMeteo.humidité++;
+    stationMeteo.temperature -= 3;
+  }
+}
+
+main();
